9029a373a23f7aea298f981e53d6c17e74f48152
jschechtman <48026389+jschechtman@users.noreply.github.com>
2019-05-02 10:48:03 -0400

    Update DoubleStack.py

--- DoubleStack.py
+++ DoubleStack.py
@@ -13,7 +13,7 @@ class DoubleStack:
     def __init__(self):
         self.data = []
         self.size = 0
-        self.last1 = None 
+        self.dividing_index = None 
     
    #Adds element of stack 1
    #to beginning of list
@@ -23,10 +23,10 @@ class DoubleStack:
         #If stack 1 was empty, set last1
         #to first element of list. Otherwise 
         #increment it.
-        if self.last1 == None:
-            self.last1 = 0
+        if self.dividing_index == None:
+            self.dividing_index = 0
         else:
-            self.last1 += 1
+            self.dividing_index += 1
     
     #Add element to Stack 2 at end of list
     #and increment size
@@ -37,23 +37,23 @@ class DoubleStack:
     #Remove element of stack 1 from start of list
     def pop1(self):
         #If stack 1 is empty return none
-        if self.last1 == None:
+        if self.dividing_index == None:
             return None
         #Pop first element of list
         else:
             oldVal = self.data[0]
             self.data.pop(0)
             self.size -= 1
-            self.last1 -= 1
+            self.dividing_index -= 1
             #Set last1 to none if
             #stack 1 is empty
-            if self.last1 < 0:
-                self.last1 = None
+            if self.dividing_index < 0:
+                self.dividing_index = None
             return oldVal
                 
     def pop2(self):
         #Empty stack 2: return none
-        if self.size == 0 or self.last1 == self.size-1:
+        if self.size == 0 or self.dividing_index == self.size-1:
             return None
         #Non-empty stack,empty stack1: remove last 
         #value, de-increment size
@@ -63,13 +63,13 @@ class DoubleStack:
             self.size -= 1
     
     def peek1():
-        if self.last1 == None: #If stack 1 is empty
+        if self.dividing_index == None: #If stack 1 is empty
             return None
         else:
             return self.data[self.last1]
         
     def peek2():
-        if self.size == 0 or self.last1 == self.size-1: #Empty stack 2
+        if self.size == 0 or self.dividing_index == self.size-1: #Empty stack 2
             return None
         else: 
             return self.data[self.size-1]
